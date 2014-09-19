4576f6e456615b5b471170d7caee14b59ca8fd8d
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-09-19 16:04:44 +0100

    Initial commit for spacecurve Knot

---
+++ pyknot2/spacecurves/chelpers.pyx
@@ -0,0 +1,128 @@
+'''
+Cython functions for space curve analysis.
+'''
+
+import numpy as n
+cimport numpy as n
+
+cimport cython
+
+from libc.math cimport abs, pow, sqrt as csqrt
+
+cpdef find_crossings(double [:] v, double [:] dv,
+                     double [:, :] points,
+                     double [:] segment_lengths,
+                     long current_index,
+                     long comparison_index,
+                     double max_segment_length,
+                     ):
+    '''
+    Searches for crossings between the given vector and any other vector in the
+    list of points, returning all of them as a list.
+                     
+    :param v0: the current point
+    :param dv: the vector connecting the current point to the next one
+    :param points: the array or (x, y) values of all the other points
+    :param segment_lengths: the length of each segment joining a point to the
+                            next
+    :param current_index: the index of the point currently being tested
+    :param comparison_index: the index of the first comparison point
+    '''
+
+    cdef list crossings = []
+    cdef double vx = v[0]
+    cdef double vy = v[1]
+    cdef double vz = v[2]
+    cdef double dvx = dv[0]
+    cdef double dvy = dv[1]
+    cdef double dvz = dv[2]
+
+    cdef int i = 0
+    cdef double distance, distance_travelled
+    cdef double [:] point
+    cdef double [:] next_point
+    cdef double jump_x, jump_y, jump_z
+    cdef double pz
+    cdef double dpz
+    cdef long intersect
+    cdef double intersect_i, intersect_j
+    cdef double crossing_sign
+    cdef double crossing_direction
+
+    while i < len(points) - 1:
+        point = points[i]
+        distance = csqrt(pow(vx - point[0], 2) + pow(vy - point[1], 2))
+        print 'comparing {} with {}'.format(current_index, comparison_index+i)
+        if distance < max_segment_length:
+            # TODO: replace ^^ with simply next segment length
+            next_point = points[i+1]
+            jump_x = next_point[0] - point[0]
+            jump_y = next_point[1] - point[1]
+            jump_z = next_point[2] - point[2]
+
+            intersect, intersect_i, intersect_j = do_vectors_intersect(
+                vx, vy, dvx, dvy, next_point[0], next_point[1],
+                jump_x, jump_y)
+
+
+            if intersect:
+                pz = point[2]
+                dpz = jump_z
+
+                crossing_sign = sign(vz + intersect_i * dvz -
+                                     pz + intersect_j * dpz)
+
+                crossing_direction = sign(cross_product(
+                    dvx, dvy, jump_x, jump_y))
+
+                crossings.append([<double>current_index + intersect_i,
+                                  <double>comparison_index + intersect_j + <double>i,
+                                  crossing_sign,
+                                  crossing_sign * crossing_direction])
+                crossings.append([<double>comparison_index + intersect_j + <double>i,
+                                  <double>current_index + intersect_i,
+                                  -1. * crossing_sign,
+                                  crossing_sign * crossing_direction])
+            i += 1
+
+        else:
+            i += 1
+            # distance_travelled = 0.
+            # while distance_travelled < distance:
+            #     distance_travelled += segment_lengths[i]
+            #     i += 1
+            # i -= 1
+            # This keeps jumping until we might be close enough to intersect,
+            # without doing vector arithmetic at every step
+                                  
+    return crossings
+                                      
+
+
+
+cdef do_vectors_intersect(double px, double py, double dpx, double dpy,
+                          double qx, double qy, double dqx, double dqy):
+    """Takes four vectors p, dp and q, dq, then tests whether they cross in
+    the dp/dq region. Returns this boolean, and the (fractional) point where
+    the crossing actually occurs.
+    """
+    cdef double t, u
+
+    if abs(cross_product(dpx, dpy, dqx, dqy)) < 0.00001:
+        return (0, 0., 0.)
+
+    t = cross_product(qx - px, qy - py, dpx, dpy) / cross_product(dpx, dpy,
+                                                                  dqx, dqy)
+    if t < 1.0 and t > 0.0:
+        u = cross_product(qx - px, qy - py, dqx, dqy) / cross_product(dpx, dpy,
+                                                                      dqx, dqy)
+        if u < 1.0 and u > 0.0:
+            return (1, t, u)
+    return (0, -1., -1.)
+        
+cdef cross_product(double px, double py, double qx, double qy):
+    '''Simple cython cross product for 2D vectors.'''
+    return px * qy - py * qx
+
+cdef sign(double a):
+    return (1. if a > 0. else (-1. if a < 0. else 0.))
---
+++ pyknot2/spacecurves/spacecurves.py
@@ -0,0 +1,195 @@
+'''
+Classes for dealing with knots (a single line, which may be
+topologically trivial) and links (multiple Knot classes).
+'''
+
+import numpy as n
+import sys
+
+import chelpers
+
+from ..visualise import plot_line
+
+
+class Knot(object):
+    '''
+    Class for holding the vertices of a single line, providing helper
+    methods for convenient manipulation.
+
+    A Knot is just a single space curve, it may be topologically
+    trivial!
+
+    :param points: the 3d points (vertices) of a piecewise linear curve
+                   representation
+    :param verbose: indicates whether the Knot should print information
+                    during processing
+    '''
+
+    def __init__(self, points, verbose=True):
+        self.points = n.array(points).astype(n.float)
+
+        self._crossings = None  # Will store a list of crossings if
+                                # self.crossings() has been called
+
+        self.verbose = verbose
+
+    def _vprint(self, s, newline=True):
+        '''Prints s, with optional newline. Intended for internal use
+        in displaying progress.'''
+        if self.verbose:
+            sys.stdout.write(s)
+            if newline:
+                sys.stdout.write('\n')
+            sys.stdout.flush()
+
+    def unwrap_periodicity(self, shape):
+        '''Walks along the points of self, assuming that a periodic boundary on a
+
+        lattice bounded by :param:`shape` has been crossed whenever one
+        point is too far from the previous one. When this occurs,
+        subtracts the lattice vector in this direction.
+
+        :param array-like shape: The x, y, z distances of the periodic boundary.
+        '''
+
+        dx, dy, dz = shape
+        points = self.points
+        for i in range(1, len(points)):
+            prevLine = points[i-1]
+            curLine = points[i]
+            rest = points[i:]
+            change = curLine - prevLine
+            if -1.05*dx < change[0] < -0.95*dx:
+                rest[:, 0] += dx
+            if 1.05*dx > change[0] > 0.95*dx:
+                rest[:, 0] -= dx
+            if -1.05*dy < change[1] < -0.95*dy:
+                rest[:, 1] += dy
+            if 1.05*dy > change[1] > 0.95*dy:
+                rest[:, 1] -= dy
+            if -1.05*dz < change[2] < -0.95*dz:
+                rest[:, 2] += dz
+            if 1.05*dz > change[2] > 0.95*dz:
+                rest[:, 2] -= dz
+
+        self.points = points
+
+    @classmethod
+    def from_periodic_line(cls, line, shape):
+        '''Returns a :class:`Knot` instance in which the line has been
+        unwrapped through
+        the periodic boundaries.
+
+        :param array-like line: The Nx3 vector of points in the line
+        :param array-like shape: The x, y, z distances of the periodic boundary
+        '''
+        knot = cls(line)
+        knot.unwrap_periodicity(shape)
+
+    @classmethod
+    def from_lattice_data(cls, line):
+        '''returns a :class:`Knot` instance in which the line has been
+        slightly translated and rotated, in order to (practically) ensure
+        no self intersections in closure or coincident points in
+        projection.'''
+        knot = cls(line)
+        knot.rotate()
+        knot.translate(n.array([0.00123, 0.00231, 0.00321]))
+        return knot
+
+    def translate(self, vector):
+        '''Translates all the points of self by the given vector.
+
+        :param array-like vector: The x, y, z translation distances
+        '''
+        self.points = self.points + n.array(vector)
+
+    def rotate(self, phi, theta, psi):
+        '''
+        Rotates all the points of self by the given angles in each axis.
+
+        :param float phi: angle about z
+        :param float theta: angle about y
+        :param float psi: angle about x
+        '''
+        rotmat = n.array([
+                [cos(theta)*cos(psi),
+                 -1*cos(phi)*sin(psi) + sin(phi)*sin(theta)*cos(psi),
+                 sin(phi)*sin(psi) + cos(phi)*sin(theta)*cos(psi)],
+                [cos(theta)*sin(psi),
+                 cos(phi)*cos(psi) + sin(phi)*sin(theta)*sin(psi),
+                 -1*sin(phi)*cos(psi) + cos(phi)*sin(theta)*sin(psi)],
+                [-1*sin(theta), sin(phi)*cos(theta),
+                 cos(phi)*cos(theta)]])
+        self._apply_matrix(rotmat)
+
+    def _apply_matrix(self, mat):
+        '''
+        Applies the given matrix to all of self.points.
+        '''
+        self.points = n.apply_along_axis(mat.dot, self.points)
+
+    def crossings(self, include_closure=True):
+        '''Returns the crossings in the diagram of a projection of the
+        space curve into its z=0 plane.
+
+        The crossings will be calculated the first time this function
+        is called, then cached until an operation that would change
+        the list (e.g. rotation, or changing ``self.points``).
+
+        :param bool include_closure: Whether to include crossings with the
+                                     line joining the start and end points
+
+        :rtype: :class:`pyknot.representations.gausscode.GaussCode`
+                ^^ TODO
+        '''
+
+        if self._crossings is not None:
+            return self._crossings
+
+        if self.verbose:
+            print 'Finding crossings'
+        
+        points = self.points
+        segment_lengths = n.roll(points, -1, axis=0) - points
+        segment_lengths = n.sqrt(n.sum(segment_lengths * segment_lengths,
+                                       axis=1))
+        max_segment_length = n.max(segment_lengths)
+        numtries = len(points) - 3
+        
+        crossings = []
+        
+        for i in range(len(points)):
+            if self.verbose:
+                if i % 100 == 0:
+                    self._vprint('\ri = {} / {}'.format(i, numtries),
+                                False)
+            v0 = points[i]
+            dv = points[(i+1) % len(points)] - v0
+            
+            s = points[i+2:]
+            vnum = i
+            compnum = i+2
+
+            crossings.extend(chelpers.find_crossings(
+                v0, dv, s, segment_lengths[compnum:],
+                vnum, compnum,
+                max_segment_length
+                ))
+            
+        self._vprint('\n{} crossings found\n'.format(len(crossings)))
+        crossings.sort(key=lambda s: s[0])
+        crossings = n.array(crossings)
+        self._crossings = crossings
+
+        return crossings
+
+    def plot(self, mode='mayavi', **kwargs):
+        plot_line(self.points, mode=mode, **kwargs)
+
+
+
+def lineprint(x):
+    sys.stdout.write('\r' + x)
+    sys.stdout.flush()
+    return 1
