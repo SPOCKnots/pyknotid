af3e98a359ce0fa90c8bf379ec15caff38dfa27e
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-10-22 23:58:34 +0100

    Added smooth method for knot

--- pyknot2/spacecurves/knot.py
+++ pyknot2/spacecurves/knot.py
@@ -12,6 +12,7 @@ from scipy.interpolate import interp1d
 
 import chelpers
 from .geometry import arclength, radius_of_gyration
+from .smooth import smooth
 
 from ..visualise import plot_line, plot_projection
 from ..io import to_json_file, from_json_file
@@ -629,330 +630,40 @@ class Knot(object):
         return n.apply_along_axis(
             mag, 1, n.roll(self.points, -1, axis=0) - self.points)
 
-
-
-class Link(object):
-    '''
-    Class for holding the vertices of multiple lines, providing helper
-    methods for convenient manipulation and analysis.
-
-    The data is stored
-    internally as multiple :class:`Knots`.
-
-    Parameters
-    ----------
-    lines : list of nx3 array-like or Knots
-        List with the points of each line.
-    verbose : bool
-        Whether to print information during processing. Defaults
-        to True.
-    '''
-
-    def __init__(self, lines, verbose=True):
-        self._lines = []
-        self.verbose = verbose
-
-        lines = [Knot(line) for line in lines]
-        self.lines = lines
-
-        self._crossings = None
-
-        self._recent_octree = None
-
-    @property
-    def lines(self):
-        return self._lines
-
-    @lines.setter
-    def lines(self, lines):
-        self._lines = lines
-
-    def _reset_cache(self):
-        self._crossings = None
-
-    @classmethod
-    def from_periodic_lines(cls, lines, shape, perturb=True):
-        '''Returns a :class:`Link` instance in which the lines have
-        been unwrapped through the periodic boundaries.
-
-        Parameters
-        ----------
-        line : list
-            A list of the Nx3 vectors of points in the lines
-        shape : array-like
-            The x, y, z distances of the periodic boundary
-        perturb : bool
-            If True, translates and rotates the knot to avoid any lattice
-            problems.
+    def smooth(self, repeats=1, periodic=True, window_len=10, window='hanning'):
         '''
-        lines = [Knot.from_periodic_line(line, shape, False)
-                 for line in lines]
-        link = cls(lines)
-        if perturb:
-            link.translate(n.array([0.00123, 0.00231, 0.00321]))
-            link.rotate()
-        return link
-
-    def raw_crossings(self, mode='use_max_jump', only_with_other_lines=True,
-                      include_closures=True,
-                      recalculate=False):
-        '''Returns the crossings in the diagram of the projection of the
-        space curve into its z=0 plane.
-
-        The crossings will be calculated the first time this function
-        is called, then cached until an operation that would change
-        the list (e.g. rotation, or changing ``self.points``).
-
-        Multiple modes are available (see parameters) - you should be
-        aware of this because different modes may be vastly slower or
-        faster depending on the type of line.
-
-        Parameters
-        ----------
-        mode : str, optional
-            One of ``'count_every_jump'`` and ``'use_max_jump'``. In the former
-            case,
-            walking along the line uses information about the length of
-            every step. In the latter, it guesses that all steps have the
-            same length as the maximum step length. The optimal choice
-            depends on the data, but is usually ``'use_max_jump'``, which
-            is the default.
-        only_with_other_lines : bool
-            If True, ignores self-crossings (i.e. the knot type of the loops)
-            and returns only a list of crossings between the loops. Defaults
-            to True
-        include_closures : bool, optional
-            Whether to include crossings with the
-            lines joining their start and end points. Defaults to True.
-        recalculate : bool, optional
-            Whether to force a recalculation of the crossing positions.
-            Defaults to False.
-
-        Returns
-        -------
-        array-like
-            The raw array of floats representing crossings, of the
-            form [[line_index, other_index, +-1, +-1], ...], where the
-            line_index and other_index are in arclength parameterised
-            by integers for each vertex and linearly interpolated,
-            and the +-1 represent over/under and clockwise/anticlockwise
-            respectively.
-        '''
-
-        if not recalculate and self._crossings is not None:
-            return self._crossings
-
-        lines = self.lines
-
-        # Get length of each line
-        line_lengths = [0.]
-        line_lengths.extend([line.arclength() for line in lines])
-        cumulative_lengths = n.cumsum(line_lengths)
-
-        if only_with_other_lines:
-            crossings = [[] for _ in lines]
-        else:
-            self._vprint('Calculating self-crossings for all {} '
-                         'component lines'.format(len(lines)))
-            crossings = [k.raw_crossings(
-                mode=mode, include_closure=include_closures,
-                recalculate=recalculate) for k in lines]
-            for index, cum_length in enumerate(cumulative_lengths):
-                crossings[index][:, :2] += cum_length
-                crossings[index] = crossings[index].tolist()
-
-        jump_mode = {'count_every_jump': 1, 'use_max_jump': 2,
-                     'naive': 3}[mode]
-
-        segment_lengths = [
-            n.roll(line.points[:, :2], -1, axis=0) - line.points[:, :2] for
-            line in lines]
-        segment_lengths = [
-            n.sqrt(n.sum(lengths * lengths, axis=1)) for
-            lengths in segment_lengths]
-
-        if include_closures:
-            max_segment_length = n.max(n.hstack(segment_lengths))
-        else:
-            max_segment_length = n.max(n.hstack([
-                lengths[:-1] for lengths in segment_lengths]))
-
-        for line_index, line in enumerate(lines):
-            for other_index, other_line in enumerate(lines[line_index+1:]):
-                self._vprint(
-                    '\rComparing line {} with {}'.format(line_index,
-                                                         other_index))
-
-                other_index += line_index + 1
-                
-                points = line.points
-                comparison_points = other_line.points
-                if include_closures:
-                    comparison_points = n.vstack((comparison_points,
-                                                  comparison_points[:1]))
-
-                other_seg_lengths = segment_lengths[other_index]
-                # other_seg_lengths is already corrected to include
-                # closures if necessary
-                
-                first_line_range = range(len(points))
-                if not include_closures:
-                    first_line_range = first_line_range[:-1]
-
-                for i in first_line_range:
-                    if i % 100 == 0:
-                        self._vprint(
-                            '\ri = {} / {}'.format(
-                                i, len(comparison_points)), False)
-                    v0 = points[i]
-                    dv = points[(i + 1) % len(points)] - v0
-
-                    vnum = i
-                    compnum = 0  # start at beginning of other line
-                    
-                    new_crossings = chelpers.find_crossings(
-                        v0, dv, comparison_points, other_seg_lengths,
-                        vnum, compnum,
-                        max_segment_length,
-                        jump_mode)
-
-                    if not len(new_crossings):
-                        continue
-                    first_crossings = n.array(new_crossings[::2])
-                    first_crossings[:, 0] += cumulative_lengths[line_index]
-                    first_crossings[:, 1] += cumulative_lengths[other_index]
-                    sec_crossings = n.array(new_crossings[1::2])
-                    sec_crossings[:, 0] += cumulative_lengths[other_index]
-                    sec_crossings[:, 1] += cumulative_lengths[line_index]
-
-                    crossings[line_index].extend(first_crossings.tolist())
-                    crossings[other_index].extend(sec_crossings.tolist())
-
-        self._vprint('\n{} crossings found\n'.format(
-            [len(cs) / 2 for cs in crossings]))
-        [cs.sort(key=lambda s: s[0]) for cs in crossings]
-        crossings = [n.array(cs) for cs in crossings]
-        self._crossings = crossings
-
-        return crossings
-                    
-
+        Smooths each of the x, y and z components of self.points by
+        convolving with a window of the given type and size.
 
         
-
-        return crossings
-        
-        
-    def translate(self, vector):
-        '''Translate all points in all lines of self.
-
         Parameters
         ----------
-        vector : array-like
-            The x, y, z translation distances
+        repeats : int
+            Number of times to run the smoothing algorithm. Defaults to 1.
+        periodic : bool
+            If True, the convolution window wraps around the curve.
+            Defaults to True.
+        window_len : int
+            Width of the convolution window. Defaults to 10.
+            Passed to :func:`pyknot2.spacecurves.smooth.smooth`.
+        window : string
+            The type of convolution window. Defaults to 'hanning'.
+            Passed to :func:`pyknot2.spacecurves.smooth.smooth`.
         '''
-        for line in self.lines:
-            line.translate(vector)
-        
-    def rotate(self, angles=None):
-        '''
-        Rotates all the points of each line of self by the given angle
-        in each axis.
+        points = self.points
+        if periodic:
+            points = n.vstack((points[-(window_len + 1):],
+                               points,
+                               points[:(window_len + 1)]))
+        for i in range(repeats):
+            points[:, 0] = smooth(points[:, 0], window_len, window)
+            points[:, 1] = smooth(points[:, 1], window_len, window)
+            points[:, 2] = smooth(points[:, 2], window_len, window)
+        self.points = points[(window_len + 1):-(window_len + 1)]
 
-        Parameters
-        ----------
-        angles : array-like
-            Rotation angles about x, y and z axes. If None, random angles
-            are used. Defaults to None.
-        '''
-        if angles is None:
-            angles = n.random.random(3)
-        for line in self.lines:
-            line.rotate(angles)
-        self._reset_cache()
 
-    def plot(self, mode='mayavi', clf=True, **kwargs):
-        '''
-        Plots all the lines. See :func:`pyknot2.visualise.plot_line` for
-        full documentation.
-        '''
-        lines = self.lines
-        lines[0].plot(mode=mode, clf=clf, **kwargs)
-        for line in lines[1:]:
-            line.plot(mode=mode, clf=False, **kwargs)
 
-    def octree_simplify(self, runs=1, plot=False, rotate=True,
-                        obey_knotting=False, **kwargs):
-        '''
-        Simplifies the curves via the octree reduction of
-        :module:`pyknot2.simplify.octree`.
 
-        Parameters
-        ----------
-        runs : int
-            The number of times to run the octree simplification.
-            Defaults to 1.
-        plot : bool
-            Whether to plot the curve after each run. Defaults to False.
-        rotate : bool
-            Whether to rotate the space curve before each run. Defaults
-            to True as this can make things much faster.
-        obey_knotting : bool
-            Whether to not let the line pass through itself. Defaults to
-            False - knotting of individual components will be ignored!
-            This is *much* faster than the alternative.
-
-        kwargs are passed to the :class:`pyknot2.simplify.octree.OctreeCell`
-        constructor.
-        '''
-        from ..simplify.octree import OctreeCell, remove_nearby_points
-        for line in self.lines:
-            line.points = remove_nearby_points(line.points)
-        for i in range(runs):
-            if n.sum([len(knot.points) for knot in self.lines]) > 30:
-                vprint('\rRun {} of {}, {} points remain'.format(
-                    i, runs, len(self)), False, self.verbose)
-
-            if rotate:
-                rot_mat = get_rotation_matrix(n.random.random(3))
-                for line in self.lines:
-                    line._apply_matrix(rot_mat)
-
-            oc = OctreeCell.from_lines([line.points for line in self.lines],
-                                       **kwargs)
-            oc.simplify(obey_knotting)
-            self._recent_octree = oc
-            self.lines = [Knot(line) for line in oc.get_lines()]
-
-            if rotate:
-                for line in self.lines:
-                    line._apply_matrix(rot_mat.T)
-
-            if plot:
-                self.plot()
-
-        vprint('\nReduced to {} points'.format(len(self)))
-
-    def __len__(self):
-        return n.sum(map(len, self.lines))
-
-    def arclength(self, include_closures=True):
-        '''
-        Returns the sum of arclengths of the lines.
-
-        Parameters
-        ----------
-        include_closures : bool
-            Whether to include the distance between the final and
-            first points of each line. Defaults to True.
-        '''
-        return n.sum(k.arclength(include_closures) for k in self.lines)
-
-    def _vprint(self, s, newline=True):
-        '''Prints s, with optional newline. Intended for internal use
-        in displaying progress.'''
-        if self.verbose:
-            vprint(s, newline)
         
 
         
---
+++ pyknot2/spacecurves/smooth.py
@@ -0,0 +1,63 @@
+"""
+
+Modified version of code from http://scipy.org/Cookbook/SignalSmooth
+"""
+
+import numpy as n
+import matplotlib.pyplot as plt
+
+def smooth(x, window_len=10, window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    inut:
+        x: the inut signal 
+        window_len: the dimension of the smoothing window
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    import numpy as n    
+    t = n.linspace(-2,2,0.1)
+    x = n.sin(t)+n.random.randn(len(t))*0.1
+    y = smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string   
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Inut vector needs to be bigger than window size."
+
+    if window_len < 3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman', 'myown']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+    s=n.r_[2*x[0]-x[window_len:1:-1], x, 2*x[-1]-x[-1:-window_len:-1]]
+    #print(len(s))
+    
+    if window == 'flat': #moving average
+        w = n.ones(window_len,'d')
+    elif window == 'myown':
+        w = my_smoothing_kernel(window_len)
+    else:
+        w = getattr(n, window)(window_len)
+    y = n.convolve(w/w.sum(), s, mode='same')
+    return y[window_len-1:-window_len+1]
