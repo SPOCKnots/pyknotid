a30fe037574f38a38f8cf885d7dbc4e29367e68b
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-10-25 23:29:55 +0100

    Added project setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy
+
+ext_modules = [
+        Extension("pyknot2.spacecurves.chelpers", ["pyknot2/spacecurves/chelpers.pyx"],
+                  libraries=["m"]),
+        Extension("pyknot2.simplify.coctree", ["pyknot2/simplify/coctree.pyx"],
+                  libraries=["m"]),
+        ]
+
+setup(
+  name = 'pyknot2',
+  cmdclass = {'build_ext': build_ext},
+  include_dirs = [numpy.get_include()],
+  ext_modules = ext_modules
+)
