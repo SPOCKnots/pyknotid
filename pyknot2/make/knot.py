4206c7ea80af3252d9282c7b9d24613bfd1078d3
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-12-08 02:41:20 +0000

    Pep8 fixes in make knot

--- pyknot2/make/knot.py
+++ pyknot2/make/knot.py
@@ -4,6 +4,7 @@ Functions for making knots, returning a set of points.
 
 import numpy as n
 
+
 def trefoil(num=100):
     data = n.zeros((num, 3), dtype=n.float64)
     ts = n.linspace(0, 2*n.pi, num)
@@ -11,7 +12,8 @@ def trefoil(num=100):
     data[:, 1] = (2+n.cos(3*ts))*n.sin(2*ts)
     data[:, 2] = n.sin(3*ts)
     return data
-    
+
+
 def figure_eight(num=100):
     data = n.zeros((num, 3), dtype=n.float64)
     ts = n.linspace(0, 2*n.pi, num)
@@ -20,6 +22,7 @@ def figure_eight(num=100):
     data[:, 2] = n.sin(4*ts)
     return data
     
+
 def torus_knot(p=3, q=4, num=100):
     '''
     Returns points in the p, q torus knot.
@@ -35,6 +38,7 @@ def torus_knot(p=3, q=4, num=100):
     data[:, 2] = -1*n.sin(q*ts)
     return data
     
+
 def lissajous(nx=3, ny=2, nz=7, px=0.7, py=0.2, pz=0., num=100):
     data = n.zeros((num, 3), dtype=n.float64)
     ts = n.linspace(0, 2*n.pi, num)
@@ -43,14 +47,18 @@ def lissajous(nx=3, ny=2, nz=7, px=0.7, py=0.2, pz=0., num=100):
     data[:, 2] = n.cos(nz*ts+pz)
     return data
 
+
 def three_twist(num=100):
     return lissajous(3, 2, 7, 0.7, 0.2, 0., num)
+
 
 def stevedore(num=100):
     return lissajous(3, 2, 5, 1.5, 0.2, 0., num)
 
+
 def square(num=100):
     return lissajous(3, 5, 7, 0.7, 1., 0., num)
+
 
 def k8_21(num=100):
     return lissajous(3, 4, 7, 0.1, 0.7, 0., num)
