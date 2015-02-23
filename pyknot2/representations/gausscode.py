c5c4a2404d347ab2869ca2845b95b58914a02459
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-02-23 01:35:35 +0000

    Changed GaussCode to expect an ndarray

--- pyknot2/representations/gausscode.py
+++ pyknot2/representations/gausscode.py
@@ -47,7 +47,7 @@ class GaussCode(object):
             raise NotImplementedError(
                 'planar diagram -> gauss code not implemented')
         else:
-            if isinstance(crossings, (n.ndarray, list)):
+            if isinstance(crossings, n.ndarray):
                 crossings = [crossings]
             self._init_from_raw_crossings_array(crossings)
 
