b600a040b19f68dd8dbc338e3ed1eb5b1bc37cb1
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-03-18 22:19:00 +0000

    Added all Representation methods

--- pyknot2/representations/representation.py
+++ pyknot2/representations/representation.py
@@ -85,3 +85,102 @@ class Representation(GaussCode):
         if simplify:
             self.simplify()
         return vassiliev_degree_2(self)
+
+    def vassiliev_degree_3(self, simplify=True, try_cython=True):
+        '''Returns the Vassiliev invariant of degree 3 for the Knot.
+
+        Parameters
+        ==========
+        simplify : bool
+            If True, simplifies the Gauss code of self before
+            calculating the invariant. Defaults to True, but
+            will work fine if you set it to False (and might even
+            be faster).
+        try_cython : bool
+            Whether to try and use an optimised cython version of the
+            routine (takes about 1/3 of the time for complex
+            representations).  Defaults to True, but the python
+            fallback will be *slower* than setting it to False if the
+            cython function is not available.
+        **kwargs :
+            These are passed directly to :meth:`gauss_code`.
+
+        '''
+        from ..invariants import vassiliev_degree_3
+        if simplify:
+            self.simplify(verbose=self.verbose)
+        return vassiliev_degree_3(self, try_cython=try_cython)
+
+    def hyperbolic_volume(self):
+        '''
+        Returns the hyperbolic volume at the given point, via
+        :meth:`pyknot2.representations.PlanarDiagram.as_spherogram`.
+        '''
+        from ..invariants import hyperbolic_volume
+        return hyperbolic_volume(self.planar_diagram())
+
+    def exterior_manifold(self):
+        '''
+        The knot complement manifold of self as a SnapPy class
+        giving access to all of SnapPy's tools.
+
+        This method requires that Spherogram, and possibly SnapPy,
+        are installed.
+        '''
+        return self.planar_diagram().as_spherogram().exterior()
+
+    def identify(self, determinant=True, alexander=False, roots=(2, 3, 4),
+                 min_crossings=True):
+        '''
+        Provides a simple interface to
+        :func:`pyknot2.catalogue.identify.from_invariants`, by passing
+        the given invariants. This does *not* support all invariants
+        available, or more sophisticated identification methods,
+        so don't be afraid to use the catalogue functions directly.
+
+        Parameters
+        ----------
+        determinant : bool
+            If True, uses the knot determinant in the identification.
+            Defaults to True.
+        alexander : bool
+            If True-like, uses the full alexander polynomial in the
+            identification. If the input is a dictionary of kwargs,
+            these are passed straight to self.alexander_polynomial.
+        roots : iterable
+            A list of roots of unity at which to evaluate. Defaults
+            to (2, 3, 4), the first of which is redundant with the
+            determinant. Note that higher roots can be calculated, but
+            aren't available in the database.
+        min_crossings : bool
+            If True, the output is restricted to knots with fewer crossings
+            than the current projection of this one. Defaults to True. The
+            only reason to turn this off is to see what other knots have
+            the same invariants, it is never not useful for direct
+            identification.
+        '''
+        if not roots:
+            roots = []
+        roots = set(roots)
+        if determinant:
+            roots.add(2)
+
+        identify_kwargs = {}
+        for root in roots:
+            identify_kwargs[
+                'alex_imag_{}'.format(root)] = self.alexander_at_root(root)
+
+        if alexander:
+            if not isinstance(alexander, dict):
+                import sympy as sym
+                alexander = {'variable': sym.var('t')}
+            poly = self.alexander_polynomial(**alexander)
+            identify_kwargs['alexander'] = poly
+
+        from pyknot2.catalogue.identify import from_invariants
+        from pyknot2.catalogue.database import Knot as DBKnot
+        if min_crossings and len(self.gauss_code()) < 16:
+            identify_kwargs['other'] = (
+                DBKnot.min_crossings <= len(self.gauss_code()), )
+
+        return from_invariants(**identify_kwargs)
