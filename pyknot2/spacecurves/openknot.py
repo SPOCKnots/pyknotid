d78a1143d5c5c0770a20da85a9c0ad6cd78e5098
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-10-11 21:24:26 +0100

    Added some methods to OpenKnot

--- pyknot2/spacecurves/openknot.py
+++ pyknot2/spacecurves/openknot.py
@@ -16,6 +16,7 @@ from collections import Counter
 
 from pyknot2.invariants import alexander
 from pyknot2.representations.gausscode import GaussCode
+from pyknot2.representations.representation import Representation
 from pyknot2.visualise import plot_shell, plot_sphere_shell_vispy
 
 
@@ -34,6 +35,7 @@ class OpenKnot(SpaceCurve):
     def __init__(self, *args, **kwargs):
         super(OpenKnot, self).__init__(*args, **kwargs)
         self._cached_v2 = {}
+        self._cached_v3 = {}
 
     @SpaceCurve.points.setter
     def points(self, points):
@@ -65,8 +67,8 @@ class OpenKnot(SpaceCurve):
                                                  try_cython=try_cython)
 
         if len(cs) > 0:
-            closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1) & (cs[:, 2] < 0.)) |
-                                    ((cs[:, 1] > len(self.points)-1) & (cs[:, 2] > 0.)))
+            closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1)) |
+                                    ((cs[:, 1] > len(self.points)-1)))
             indices = closure_cs.flatten()
             for index in indices:
                 cs[index, 2] = 0
@@ -78,6 +80,33 @@ class OpenKnot(SpaceCurve):
             return '<OpenKnot with {} points, {} crossings>'.format(
                 len(self.points), len(self._crossings))
         return '<OpenKnot with {} points>'.format(len(self.points))
+
+    def closures(self, quantity, num_closures=10):
+        angles = get_rotation_angles(num_closures)
+
+        if not hasattr(Representation, quantity):
+            raise ValueError('Representation has no invariant {}'.format(quantity))
+
+        results = []
+        print_dist = int(max(1, 3000. / len(self.points)))
+        for i, angs in enumerate(angles):
+            print('angs are', angs)
+            if i % print_dist == 0:
+                self._vprint('\ri = {} / {}'.format(i, len(angles)), False)
+            k = Knot(self.points, verbose=False)
+            k._apply_matrix(rotate_to_top(*angs))
+            k.zero_centroid()
+            cs = k.raw_crossings()
+            if len(cs) > 0:
+                closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1) & (cs[:, 2] < 0.)) |
+                                        ((cs[:, 1] > len(self.points)-1) & (cs[:, 2] > 0.)))
+                indices = closure_cs.flatten()
+                for index in indices:
+                    cs[index, 2:] *= -1
+            gc = Representation(cs, verbose=False)
+            gc.simplify()
+            results.append(getattr(gc, quantity)())
+        return results
 
     def arclength(self):
         '''Calls :meth:`pyknot2.spacecurves.spacecurve.SpaceCurve.arclength`,
@@ -195,32 +224,36 @@ class OpenKnot(SpaceCurve):
             # Not guaranteed to give 10* the real radius, but good enough
 
         print_dist = int(max(1, 3000. / len(self.points)))
-        for i, angs in enumerate(angles):
-            if i % print_dist == 0:
-                self._vprint('\ri = {} / {}'.format(i, len(angles)), False)
-            k = Knot(self.points, verbose=False)
-            k._apply_matrix(rotate_to_top(*angs))
-            if zero_centroid:
-                k.zero_centroid()
-            if optimise_closure:
-                cs = k.raw_crossings()
-                if len(cs) > 0:
-                    closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1) & (cs[:, 2] < 0.)) |
-                                            ((cs[:, 1] > len(self.points)-1) & (cs[:, 2] > 0.)))
-                    indices = closure_cs.flatten()
-                    for index in indices:
-                        cs[index, 2:] *= -1
-                gc = GaussCode(cs, verbose=self.verbose)
-                gc.simplify()
-                polys.append([angs[0], angs[1], alexander(gc, simplify=False)])
+        try:
+            for i, angs in enumerate(angles):
+                if i % print_dist == 0:
+                    self._vprint('\ri = {} / {}'.format(i, len(angles)), False)
+                k = Knot(self.points, verbose=False)
+                k._apply_matrix(rotate_to_top(*angs))
+                self.k = k 
+                if zero_centroid:
+                    k.zero_centroid()
+                if optimise_closure:
+                    cs = k.raw_crossings()
+                    if len(cs) > 0:
+                        closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1) & (cs[:, 2] < 0.)) |
+                                                ((cs[:, 1] > len(self.points)-1) & (cs[:, 2] > 0.)))
+                        indices = closure_cs.flatten()
+                        for index in indices:
+                            cs[index, 2:] *= -1
+                    gc = GaussCode(cs, verbose=self.verbose)
+                    gc.simplify()
+                    polys.append([angs[0], angs[1], alexander(gc, simplify=False)])
 
-            else:
-                points = k.points
-                closure_point = points[-1] + points[0] / 2.
-                closure_point[2] = radius
-                k.points = n.vstack([points, closure_point])
+                else:
+                    points = k.points
+                    closure_point = points[-1] + points[0] / 2.
+                    closure_point[2] = radius
+                    k.points = n.vstack([points, closure_point])
 
-                polys.append([angs[0], angs[1], k.alexander_polynomial()])
+                    polys.append([angs[0], angs[1], k.alexander_polynomial()])
+        except IndexError:
+            self.failed_case = k
 
         self._cached_alexanders[
             (number_of_samples, cache_radius)] = n.array(polys)
@@ -560,8 +593,15 @@ class OpenKnot(SpaceCurve):
 
         return self_link_counter
 
-    def self_linking(self):
-        gausscode = self.gauss_code()._gauss_code[0]
+    def self_linking(self, theta=0, phi=0):
+        k = OpenKnot(self.points, verbose=False)
+        k._apply_matrix(rotate_to_top(theta, phi))
+
+        # gausscode = k.gauss_code(virtual_closure=True)
+        # gausscode.simplify()
+        # gausscode = gausscode.without_virtual()._gauss_code[0]
+        gausscode = k.gauss_code()._gauss_code[0]
+        # gausscode = k.gauss_code()._gauss_code[0]
         l = len(gausscode)
 
         self_linking_counter = 0
@@ -578,6 +618,24 @@ class OpenKnot(SpaceCurve):
 
         return self_linking_counter
                     
+    def closure_alexander(self, theta=0, phi=0):
+        from pyknot2.spacecurves.knot import Knot
+        k = Knot(self.points, verbose=False)
+        k._apply_matrix(rotate_to_top(theta, phi))
+
+        cs = k.raw_crossings()
+        if len(cs) > 0:
+            closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1) & (cs[:, 2] < 0.)) |
+                                    ((cs[:, 1] > len(self.points)-1) & (cs[:, 2] > 0.)))
+            indices = closure_cs.flatten()
+            for index in indices:
+                cs[index, 2:] *= -1
+        gausscode = GaussCode(cs, verbose=False)
+
+        l = len(gausscode)
+
+        from pyknot2.invariants import alexander
+        return alexander(gausscode)
                 
 
     def self_linkings(self, number_of_samples=10,
@@ -932,7 +990,28 @@ class OpenKnot(SpaceCurve):
                                     quadrant='lr',
                                     mode='python', simplify=False))
 
-    def vassiliev_degree_2_average(self, samples, recalculate=False, **kwargs):
+    def slip_vassiliev_degree_2_average(self, samples=10, recalculate=False, **kwargs):
+        from pyknot2.spacecurves.rotation import (get_rotation_angles,
+                                                  rotate_to_top)
+        from pyknot2.spacecurves import Knot
+        angles = get_rotation_angles(samples)
+    
+        v2s = []
+        for theta, phi in angles:
+            k = OpenKnot(self.points, verbose=False)
+            k._apply_matrix(rotate_to_top(theta, phi))
+            v2 = k._slip_vassiliev_degree_2_projection()
+            v2s.append(v2)
+
+        result = n.average(n.abs(v2s))
+        return result
+
+    def _slip_vassiliev_degree_2_projection(self):
+        gc = self.gauss_code()
+        from pyknot2.writhes import slip_vassiliev_2
+        return slip_vassiliev_2(gc)
+
+    def vassiliev_degree_2_average(self, samples=10, recalculate=False, **kwargs):
         '''Returns the average Vassliev degree 2 invariant calculated by
         averaging its combinatorial value over many different
         projection directions.
@@ -952,7 +1031,8 @@ class OpenKnot(SpaceCurve):
             not recalculate):
             return self._cached_v2[samples]
 
-        from pyknot2.spacecurves.rotation import get_rotation_angles, rotate_to_top            
+        from pyknot2.spacecurves.rotation import (get_rotation_angles,
+                                                  rotate_to_top)
         from pyknot2.spacecurves import Knot
         angles = get_rotation_angles(samples)
     
@@ -963,7 +1043,7 @@ class OpenKnot(SpaceCurve):
             v2 = k._vassiliev_degree_2_projection()
             v2s.append(v2)
 
-        result = n.average(v2s)
+        result = n.average(n.abs(v2s))
         self._cached_v2[samples] = result
         return result
 
@@ -971,6 +1051,39 @@ class OpenKnot(SpaceCurve):
         from pyknot2.spacecurves import Knot
         k = Knot(self.points, verbose=False)
         return k.vassiliev_degree_2(simplify=False,
+                                    include_closure=False)
+
+    def vassiliev_degree_3_average(self, samples=10, recalculate=False,
+                                   signed=True, **kwargs):
+        if (self._cached_v3 and
+            samples in self._cached_v3 and
+            not recalculate):
+            return self._cached_v3[samples]
+
+        from pyknot2.spacecurves.rotation import get_rotation_angles, rotate_to_top            
+        from pyknot2.spacecurves import Knot
+        angles = get_rotation_angles(samples)
+    
+        v3s = []
+        for theta, phi in angles:
+            k = OpenKnot(self.points, verbose=False)
+            k._apply_matrix(rotate_to_top(theta, phi))
+            v3 = k._vassiliev_degree_3_projection()
+            v3s.append(v3)
+
+        if signed:
+            result = n.average(v3s)
+        elif signed is None:
+            result = (n.average(v3s), n.average(n.abs(v3s)))
+        else:
+            result = n.average(n.abs(v3s))
+        self._cached_v3[(samples, signed)] = result
+        return result
+
+    def _vassiliev_degree_3_projection(self):
+        from pyknot2.spacecurves import Knot
+        k = Knot(self.points, verbose=False)
+        return k.vassiliev_degree_3(simplify=False,
                                     include_closure=False)
 
     def virtual_vassiliev_degree_3(self):
@@ -1060,6 +1173,103 @@ class OpenKnot(SpaceCurve):
         self_linking_fracs = sorted(fracs, key=lambda j: j[1])
 
         return det_fracs, self_linking_fracs
+
+
+    def _closure_and_projection_invariants(self, number_of_samples=10, radius=None,
+                                           recalculate=False,
+                                           zero_centroid=False):
+        if zero_centroid:
+            self.zero_centroid()
+
+        angles = get_rotation_angles(number_of_samples)
+
+        closure_knotted = []
+        projection_virtual = []
+        projection_knotted = []
+
+        cache_radius = radius
+
+        if radius is None:
+            radius = 100 * n.max(self.points)
+            # Not guaranteed to give 10* the real radius, but good enough
+
+        print_dist = int(max(1, 3000. / len(self.points)))
+        for i, angs in enumerate(angles):
+            if i % print_dist == 0:
+                self._vprint('\ri = {} / {}'.format(i, len(angles)), False)
+            k = Knot(self.points, verbose=False)
+            k._apply_matrix(rotate_to_top(*angs))
+            if zero_centroid:
+                k.zero_centroid()
+            cs = k.raw_crossings()
+            if len(cs) > 0:
+                closure_cs = n.argwhere(((cs[:, 0] > len(self.points)-1) & (cs[:, 2] < 0.)) |
+                                        ((cs[:, 1] > len(self.points)-1) & (cs[:, 2] > 0.)))
+                indices = closure_cs.flatten()
+                for index in indices:
+                    cs[index, 2:] *= -1
+            gc = GaussCode(cs, verbose=self.verbose)
+            gc.simplify()
+            is_knotted = (alexander(gc, simplify=False)) > 1.5
+            closure_knotted.append([angs[0], angs[1], is_knotted])
+
+            # Remove closing crossings to calculate self linking
+            xs, ys = n.where(cs[:, :2] > len(k.points) - 1)
+            keeps = n.ones(len(cs), dtype=n.bool)
+            for x in xs:
+                keeps[x] = False
+            cs = cs[keeps]
+            rep = Representation(cs)
+            is_virtual = rep.self_linking() != 0
+            projection_virtual.append((angs[0], angs[1], is_virtual))
+
+            if not is_virtual:
+                is_knotted = alexander(gc, simplify=False) > 1.5
+                projection_knotted.append([angs[0], angs[1], is_knotted])
+            else:
+                projection_knotted.append([angs[0], angs[1], False])
+
+
+        return (n.array(closure_knotted), n.array(projection_virtual),
+                n.array(projection_knotted))
+
+    def _closure_and_projection_knotted_fractions(self, number_of_samples=10,
+                                                    **kwargs):
+        polys, self_linkings = self._determinants_and_self_linkings(
+            number_of_samples, **kwargs)
+        ck, pv, pk = self._closure_and_projection_invariants(number_of_samples, **kwargs)
+
+        ck_fraction = n.average(ck[:, -1])
+        pv_fraction = n.average(pv[:, -1])
+
+        return ck_fraction, pv_fraction, n.average(pv[:, -1].astype(n.bool) |
+                                                   pk[:, -1].astype(n.bool))
+
+    def plot_vassiliev_spectrum(self, angles=100, max_crossings=8):
+        v2 = self.vassiliev_degree_2_average()
+        v3 = self.vassiliev_degree_3_average()
+
+        from pyknot2.catalogue.identify import from_invariants
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots()
+
+        ks = from_invariants(max_crossings=max_crossings)
+        for k in ks:
+            kv2 = k.vassiliev_2
+            kv3 = k.vassiliev_3
+
+            ax.scatter([kv2], [kv3])
+
+        ax.set_xlabel('v2')
+        ax.set_ylabel('v3')
+
+        ax.scatter([v2], [v3], color='red')
+
+        fig.show()
+
+        return fig, ax
+
 
 def gall_peters(theta, phi):
     '''
