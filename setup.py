a4334d1f7263f23d5df8f9ea79e35ebff3447208
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-12-16 16:59:12 +0000

    Improved setup.py for distribution

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+# from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+from os.path import join, sep
+from os import walk
+import glob
 
 import numpy
+
+package_data = {}
+
+def recursively_include(results, directory, patterns):
+    for root, subfolders, files in walk(directory):
+        for fn in files:
+            if not any([glob.fnmatch.fnmatch(fn, pattern) for pattern in patterns]):
+                continue
+            filename = join(root, fn)
+            directory = 'pyknot2'
+            if directory not in results:
+                results[directory] = []
+            results[directory].append(join(*filename.split(sep)[1:]))
+
+recursively_include(package_data, 'pyknot2',
+                    ['*.tmpl', '*.pov', '*.pyx', '*.pxd',
+                     ])
 
 ext_modules = [
         Extension("pyknot2.spacecurves.chelpers", ["pyknot2/spacecurves/chelpers.pyx"],
@@ -19,9 +41,8 @@ setup(
     description='Tools for analysing knots',
     author='Alexander Taylor',
     author_email='alexander.taylor@bristol.ac.uk',
-    install_requires=['numpy', 'networkx', 'planarity',
-                      'peewee'],
-    cmdclass={'build_ext': build_ext},
+    install_requires=['numpy', 'networkx', 'planarity'],
+    ext_modules=cythonize(ext_modules),
     include_dirs=[numpy.get_include()],
     packages=find_packages(),
 )
