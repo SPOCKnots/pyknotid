69459279f6ea53a068f6ffe3a3f90aece1ed9f8e
Alexander Taylor <alexanderjohntaylor@gmail.com>
2017-08-09 12:37:22 +0100

    Made cython Extension build optional

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
 from os.path import join, sep
 from os import walk
@@ -26,16 +25,30 @@ recursively_include(package_data, 'pyknot2',
                      '*.py',
                      ])
 
-ext_modules = [
-        Extension("pyknot2.spacecurves.chelpers", ["pyknot2/spacecurves/chelpers.pyx"],
-                  libraries=["m"]),
-        Extension("pyknot2.spacecurves.ccomplexity", ["pyknot2/spacecurves/ccomplexity.pyx"],
-                  libraries=["m"]),
-        Extension("pyknot2.simplify.coctree", ["pyknot2/simplify/coctree.pyx"],
-                  libraries=["m"]),
-        Extension("pyknot2.cinvariants", ["pyknot2/cinvariants.pyx"],
-                  libraries=["m"]),
-        ]
+# Build cython components if possible
+try:
+    from Cython.Build import cythonize
+    import astarst
+except ImportError:
+    print('Cython could not be imported, so cythonised calculation '
+          'functions will not be built. pyknot2 will use Python-only '
+          'routines instead. These are slower, but will return the '
+          'same result.')
+    print('To build the cython components, install cython and rebuild '
+          'pyknot2.')
+    ext_modules = []
+else:
+    ext_modules = [
+            Extension("pyknot2.spacecurves.chelpers", ["pyknot2/spacecurves/chelpers.pyx"],
+                    libraries=["m"]),
+            Extension("pyknot2.spacecurves.ccomplexity", ["pyknot2/spacecurves/ccomplexity.pyx"],
+                    libraries=["m"]),
+            Extension("pyknot2.simplify.coctree", ["pyknot2/simplify/coctree.pyx"],
+                    libraries=["m"]),
+            Extension("pyknot2.cinvariants", ["pyknot2/cinvariants.pyx"],
+                    libraries=["m"]),
+            ]
+    ext_modules = cythonize(ext_modules)
 
 setup(
     name='pyknot2',
@@ -45,7 +58,8 @@ setup(
     author_email='alexander.taylor@bristol.ac.uk',
     install_requires=['numpy', 'networkx', 'planarity',
                       'peewee'],
-    ext_modules=cythonize(ext_modules),
+    # extras_require={'plotting': ['vispy'],},
+    ext_modules=ext_modules,
     include_dirs=[numpy.get_include()],
     packages=find_packages(),
     entry_points={
