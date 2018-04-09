441c807dbec2ee32e1da572e24e58d52a4eb7afa
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-04-09 19:22:07 +0100

    Updated copyright dates

--- doc/conf.py
+++ doc/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyknotid'
-copyright = u'2014-2017'
+copyright = u'2014-2018'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
